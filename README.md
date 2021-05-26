626e23081f4e8cb3b0ff8675378038766cd59fde
Alexis Tyler <xo@wvvw.me>
2021-05-26 19:59:08 +0930

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+# <span style="font-family: 'Segoe UI Emoji'">💾</span> Lovelace Converter (UI -> YAML)
+
+[![hacs_badge](https://img.shields.io/badge/HACS-Default-orange.svg?style=for-the-badge)](https://github.com/custom-components/hacs)
+[![Open your Home Assistant instance and start setting up a new integration.](https://my.home-assistant.io/badges/config_flow_start.svg)](https://my.home-assistant.io/redirect/config_flow_start/?domain=lovelace_converter)
+
+Convert your lovelace config to YAML for easier sharing. 
+
+## Services
+
+All service parameters are optional.
+
+### lovelace_converter.convert
+
+Convert the lovelace UI config from JSON to YAML.
---
+++ custom_components/lovelace-converter/__init__.py
@@ -0,0 +1,69 @@
+"""Lovelace JSON config converter"""
+import logging
+import simplejson as json
+import yaml
+from slugify import slugify
+
+DOMAIN = "lovelace_converter"
+_LOGGER = logging.getLogger(__name__)
+
+INPUT = "/config/.storage/lovelace"
+OUTPUT = "/config/ui-lovelace"
+
+
+def setup(hass, config):
+    """Load lovelace JSON config from disk."""
+    def loadLoveLaceConfig(filePath):
+        lovelaceFile = open(filePath, "r+")
+        data = json.load(lovelaceFile)
+        lovelaceFile.close()
+
+        return data
+
+    def without(d, key):
+        new_d = d.copy()
+        new_d.pop(key)
+        return new_d
+
+    """Write config to YAML file(s) to disk."""
+    def convertToYaml(lovelaceJson, directory):
+        lovelaceConfig = lovelaceJson["data"]["config"]
+        lovelaceMainConfig = without(lovelaceConfig, "views")
+
+        # Write the main lovelace file
+        lovelaceYaml = open(directory + ".yaml", "w+")
+        yaml.dump(lovelaceMainConfig,
+                  lovelaceYaml,
+                  default_flow_style=False)
+        # Add views include
+        lovelaceYaml.write("\nviews: !include_dir_list ui-lovelace/")
+        lovelaceYaml.close()
+
+        # Write the views
+        views = lovelaceConfig["views"]
+        for index, view in enumerate(views, start=1):
+            filename = str(index).zfill(2) + "-" + \
+                slugify(view["title"]) + ".yaml"
+            lovelaceYaml = open(directory + "/" + filename, "w+")
+            yaml.dump(view,
+                      lovelaceYaml,
+                      default_flow_style=False)
+            lovelaceYaml.close()
+
+    """Convert config to YAML."""
+    def convert(call):
+        try:
+            # Load the current lovelace config from disk
+            lovelaceJson = loadLoveLaceConfig(INPUT)
+            # Write yaml conversion to directory
+            convertToYaml(lovelaceJson, OUTPUT)
+
+            _LOGGER.info("Convertion done!")
+        except Exception as error:  # pylint: disable=broad-except
+            _LOGGER.error("Something went wrong - {}".format(error))
+
+    # Register our service with Home Assistant
+    hass.services.register(DOMAIN, 'convert', convert)
+
+    # Return boolean to indicate that initialization was successful
+    return True
